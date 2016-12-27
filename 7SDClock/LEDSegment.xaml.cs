7be9f60795aeb048f86de3dc3c82c8f7dea0fb6f
JPCoded <JPCoded@gmail.com>
2016-12-27 16:29:28 -0500

    Created Dictonaries that contain the numbers 0-9 and characters A-G that are used for holding which segments are in each number and can then be looped through to set the current number on the display.

--- 7SDClock/LEDSegment.xaml.cs
+++ 7SDClock/LEDSegment.xaml.cs
@@ -1,6 +1,9 @@
 ﻿#region
 
+using System.Collections.Generic;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 #endregion
 
@@ -11,9 +14,47 @@ namespace _7SDClock
     /// </summary>
     public partial class LedSegment : UserControl
     {
+        private readonly Dictionary<int, List<char>> _numbersDictonary = new Dictionary<int, List<char>>
+        {
+            {0, new List<char> {'A', 'B', 'C', 'D', 'E', 'F'}},
+            {1, new List<char> {'B', 'C'}},
+            {2, new List<char> {'A', 'B', 'D', 'E', 'G'}},
+            {3, new List<char> {'A', 'B', 'C', 'D', 'G'}},
+            {4, new List<char> {'B', 'C', 'F', 'G'}},
+            {5, new List<char> {'A', 'C', 'D', 'F', 'G'}},
+            {6, new List<char> {'C', 'D', 'E', 'F', 'G'}},
+            {7, new List<char> {'A', 'B', 'C'}},
+            {8, new List<char> {'A', 'B', 'C', 'D', 'E', 'F', 'G'}},
+            {9, new List<char> {'A', 'B', 'C', 'F', 'G'}}
+        };
+
+        private readonly Dictionary<char, Polygon> _segmentsDictonary;
+
         public LedSegment()
         {
             InitializeComponent();
+            _segmentsDictonary = new Dictionary<char, Polygon>
+            {
+                {'A', SegA},
+                {'B', SegB},
+                {'C', SegC},
+                {'D', SegD},
+                {'E', SegE},
+                {'F', SegF},
+                {'G', SegG}
+            };
+        }
+
+        public void SetNumber(int numberToSet)
+        {
+            if (numberToSet <= 9 && numberToSet >= 0)
+            {
+                var characters = _numbersDictonary[numberToSet];
+                foreach (var seg in _segmentsDictonary)
+                {
+                    seg.Value.Fill = characters.Contains(seg.Key) ? Brushes.Red : Brushes.LightGray;
+                }
+            }
         }
     }
 }
--- 7SDClock/MainWindow.xaml.cs
+++ 7SDClock/MainWindow.xaml.cs
@@ -11,9 +11,21 @@ namespace _7SDClock
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int currentNumber = 0;
         public MainWindow()
         {
             InitializeComponent();
+        
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentNumber > 9)
+            {
+                currentNumber = 0;
+            }
+            DigitHourTens.SetNumber(currentNumber);
+            currentNumber++;
         }
     }
 }
