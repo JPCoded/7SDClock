25517f1aa7b436324d2f3cd882e532336562e984
JPCoded <JPCoded@gmail.com>
2016-12-28 14:31:10 -0500

    Added enum to colon to make it easier to read and understand when the colons are on and off

--- 7SDClock/LEDColon.xaml.cs
+++ 7SDClock/LEDColon.xaml.cs
@@ -1,5 +1,6 @@
 ﻿#region
 
+using System;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -12,21 +13,33 @@ namespace _7SDClock
     /// </summary>
     public partial class LedColon : UserControl
     {
+        private enum ColonState
+        {
+            On,
+            Off
+        };
+        
+        private ColonState GetColonState { get; set; }
         public LedColon()
         {
             InitializeComponent();
         }
 
+  
+    
         private void TurnColonOn()
         {
             SegCol1.Fill = Brushes.Red;
             SegCol2.Fill = Brushes.Red;
+         GetColonState = ColonState.On;
         }
 
         private void TurnColonOff()
         {
+     
             SegCol1.Fill = Brushes.LightGray;
             SegCol2.Fill = Brushes.LightGray;
+            GetColonState = ColonState.Off;
         }
     }
 }
