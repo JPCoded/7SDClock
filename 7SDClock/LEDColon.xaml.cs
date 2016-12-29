6ac338a4d76dca7c68f11bdda6507a9af21da3be
JPCoded <JPCoded@gmail.com>
2016-12-29 15:42:14 -0500

    added timer to Colon segment to blink it on and off and set it to 1 second.

--- 7SDClock/LEDColon.xaml.cs
+++ 7SDClock/LEDColon.xaml.cs
@@ -1,7 +1,9 @@
 ﻿#region
 
+using System;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 #endregion
 
@@ -12,12 +14,38 @@ namespace _7SDClock
     /// </summary>
     public partial class LedColon : UserControl
     {
+        private readonly DispatcherTimer _timer = new DispatcherTimer();
+
         public LedColon()
         {
             InitializeComponent();
+            _timer.Tick += _timer_Tick;
+            _timer.Interval = new TimeSpan(0, 0, 1);
         }
 
         private ColonState GetColonState { get; set; }
+
+        public void StartColon()
+        {
+            _timer.Start();
+        }
+
+        public void StopColon()
+        {
+            _timer.Stop();
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            if (GetColonState == ColonState.On)
+            {
+                TurnColonOff();
+            }
+            else
+            {
+                TurnColonOn();
+            }
+        }
 
         private void TurnColonOn()
         {
